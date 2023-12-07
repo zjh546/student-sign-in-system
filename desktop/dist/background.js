beac3fc64c365835e32ade389c3c7046df00c14e
zjh546 <2319671152@qq.com>
2023-12-07 16:01:24 +0800

    v1.0.3 提交数据库sql 2023.12.7

--- desktop/dist/background.js
+++ desktop/dist/background.js
@@ -1,1 +1,69 @@
-var w=Object.create;var t=Object.defineProperty;var p=Object.getOwnPropertyDescriptor;var f=Object.getOwnPropertyNames;var c=Object.getPrototypeOf,u=Object.prototype.hasOwnProperty;var H=(e,n,r,a)=>{if(n&&typeof n=="object"||typeof n=="function")for(let i of f(n))!u.call(e,i)&&i!==r&&t(e,i,{get:()=>n[i],enumerable:!(a=p(n,i))||a.enumerable});return e};var M=(e,n,r)=>(r=e!=null?w(c(e)):{},H(n||!e||!e.__esModule?t(r,"default",{value:e,enumerable:!0}):r,e));var d=M(require("path")),o=require("electron");function v(){let e=new o.BrowserWindow({width:1200,height:700,minWidth:800,minHeight:500,frame:!1,webPreferences:{nodeIntegration:!0,contextIsolation:!0,webSecurity:!1,preload:d.default.resolve(__dirname,"./preload.js")}});o.Menu.setApplicationMenu(null),o.ipcMain.on("HeaderMin",()=>{e.minimize()}),o.ipcMain.on("HeaderMax",()=>{e.isMaximized()?e.restore():e.maximize()}),o.ipcMain.on("HeaderClose",()=>{e.close()}),o.ipcMain.handle("DownloadImage",(n,r,a)=>new Promise((i,l)=>{e.webContents.downloadURL(r),e.webContents.session.once("will-download",(x,s,h)=>{s.once("done",(C,m)=>{m==="completed"?i("ok"):l("error")})})})),process.argv[2]?(e.loadURL(process.argv[2]),e.webContents.openDevTools()):e.loadFile("index.html")}o.app.whenReady().then(v);o.app.on("window-all-closed",function(){process.platform!=="darwin"&&o.app.quit()});
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target, mod));
+
+// src/desktop/background.ts
+var import_path = __toESM(require("path"));
+var import_electron = require("electron");
+function createWindow() {
+  const win = new import_electron.BrowserWindow({
+    width: 1200,
+    height: 700,
+    minWidth: 800,
+    minHeight: 500,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      webSecurity: false,
+      preload: import_path.default.resolve(__dirname, "./preload.js")
+    }
+  });
+  import_electron.Menu.setApplicationMenu(null);
+  import_electron.ipcMain.on("HeaderMin" /* HeaderMin */, () => {
+    win.minimize();
+  });
+  import_electron.ipcMain.on("HeaderMax" /* HeaderMax */, () => {
+    win.isMaximized() ? win.restore() : win.maximize();
+  });
+  import_electron.ipcMain.on("HeaderClose" /* HeaderClose */, () => {
+    win.close();
+  });
+  import_electron.ipcMain.handle("DownloadImage", (event, downloadImgPath, downloadImg) => {
+    return new Promise((resolve, reject) => {
+      win.webContents.downloadURL(downloadImgPath);
+      win.webContents.session.once("will-download", (event2, item, webContents) => {
+        item.once("done", (event3, state) => {
+          if (state === "completed") {
+            resolve("ok");
+          } else {
+            reject("error");
+          }
+        });
+      });
+    });
+  });
+  if (process.argv[2]) {
+    win.loadURL(process.argv[2]);
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile("index.html");
+  }
+}
+import_electron.app.whenReady().then(createWindow);
+import_electron.app.on("window-all-closed", function() {
+  if (process.platform !== "darwin")
+    import_electron.app.quit();
+});
--- desktop/dist/preload.js
+++ desktop/dist/preload.js
@@ -1,1 +1,10 @@
-var e=require("electron");e.contextBridge.exposeInMainWorld("DeskTopHeader",{HeaderMin:()=>e.ipcRenderer.send("HeaderMin"),HeaderMax:()=>e.ipcRenderer.send("HeaderMax"),HeaderClose:()=>e.ipcRenderer.send("HeaderClose")});e.contextBridge.exposeInMainWorld("DeskMainAPI",{DownloadImage:(a,r)=>e.ipcRenderer.invoke("DownloadImage",a,r)});
+// src/desktop/preload.ts
+var import_electron = require("electron");
+import_electron.contextBridge.exposeInMainWorld("DeskTopHeader", {
+  HeaderMin: () => import_electron.ipcRenderer.send("HeaderMin" /* HeaderMin */),
+  HeaderMax: () => import_electron.ipcRenderer.send("HeaderMax" /* HeaderMax */),
+  HeaderClose: () => import_electron.ipcRenderer.send("HeaderClose" /* HeaderClose */)
+});
+import_electron.contextBridge.exposeInMainWorld("DeskMainAPI", {
+  DownloadImage: (downloadImgPath, upload_img) => import_electron.ipcRenderer.invoke("DownloadImage", downloadImgPath, upload_img)
+});
